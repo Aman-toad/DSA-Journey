73884da30fbac1a60300a42971899fa5913f6442
Aman Singh <amanaspirantsingh@gmail.com>
2025-08-10 23:15:49 +0530

    Implement various array manipulation algorithms including deletion of occurrences, even-odd separation, frequency counting, insertion with shift, pair finding with target sum, and duplicate printing. Add utility functions for array operations.

---
+++ utils/array_utils.cpp
@@ -0,0 +1,10 @@
+// functions like swap, reverse array
+#include "utils.h"
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int n){
+  for (int i = 0; i < n; i++)
+    cout<<arr[i]<<" ";
+  cout<<endl;
+}
---
+++ utils/utils.h
@@ -0,0 +1,7 @@
+// headers file to declare them
+#ifndef ARRAY_UTILS_H
+#define ARRAY_UTILS_H
+
+void printArray(int arr[], int n);
+
+#endif
