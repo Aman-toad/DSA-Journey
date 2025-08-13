990bdb3660f79817a413a5b5c11d2ff99367e1f1
Aman Singh <amanaspirantsingh@gmail.com>
2025-08-13 22:27:56 +0530

    Refactor prefix sum implementation and add range sum query functionality; create subarray sum equals k template.

--- 04_Prefix_sum/basics/range_sum_query.cpp
+++ 04_Prefix_sum/basics/range_sum_query.cpp
@@ -5,7 +5,7 @@ int main()
 {
   ios::sync_with_stdio(false);
 
-  int n;
+  int n, l, r;
   cout << "Enter array size: ";
   cin >> n;
 
@@ -16,22 +16,31 @@ int main()
     cin >> arr[i];
   }
 
-  // prefix sum array(start from 1-index for convenince)
+  // range query sum
+
+  cout << "Enter L: ";
+  cin >> l;
+  cout << "Enter R: ";
+  cin >> r;
+
   vector<int> prefix(n + 1, 0);
+  cout << "The Sum between " << l << " and " << r << " : ";
   for (int i = 1; i <= n; i++)
   {
     prefix[i] = prefix[i - 1] + arr[i - 1];
   }
+
+  int prefixSum = prefix[r] - prefix[l - 1];
+
+  cout << "\n";
 
   cout << "Original array: ";
   for (int x : arr)
     cout << x << " ";
   cout << "\n";
 
-  cout << "Prefix sum array: ";
-  for (int x : prefix)
-    cout << x << " ";
-  cout << "\n";
+  cout << "Prefix sum between " << l << " and " << r << " : ";
+  cout << prefixSum;
 
   return 0;
 }
---
+++ 04_Prefix_sum/basics/subarray_sum_equals_k.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+
+  int n,k;
+  cout << "Enter array size: ";
+  cin >> n;
+  cout<<"Enter k";cin>>k;
+
+  vector<int> arr(n);
+  cout << "Enter " << n << " elements: ";
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+
+
+}
